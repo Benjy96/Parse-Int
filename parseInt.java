1299bd74889e379e8c10c2b3ab9731469fe26f0e
Benjy96 <benjamindickey@hotmail.co.uk>
2016-10-24 11:18:12 +0100

    Update parseInt.java

--- parseInt.java
+++ parseInt.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * Take a string that holds a 'number', e.g. "5", stored as a string.
  * Cast it as an Integer, from a Character. This returns the ASCII table (decimal) value of the character 5.
  * The ASCII value of the character '5', is 53 in decimal. (35 in Hex, 065 in Oct, &#53 in Html.)
- * The number characters 0 -> 9 are represented by 48 -> 53 in decimal (ASCII table). 
+ * The number characters 0 -> 9 are represented by 48 -> 57 in decimal (ASCII table). 
  * Therefore we can subtract 48 to print the decimal value of the number.
  * e.g., Character(5) = 53 in ASCII Decimal. Subtract: 53 - 48 = 5
  * This number 5 is now able to be stored as an integer, and is the correct value.
